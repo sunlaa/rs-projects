491abe4d7c5a786e15276f27bb20c2600e61e96c
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-01-23 20:08:08 +0100

    feat: add a side menu with levels

---
+++ nonograms/levels.js
@@ -0,0 +1,70 @@
+import { create, body, table, menu, picNumber } from './script.js';
+import { answers } from './nonogram.js';
+
+const levelMenu = create('aside', 'level-menu', body);
+const complexity = create('section', 'complexity', levelMenu);
+const levelList = create('div', 'level-list', levelMenu);
+const greens = create('img', 'greens', levelMenu);
+greens.src = './images/greens.png';
+greens.alt = 'Greens';
+
+const easy = create('div', 'easy grade', complexity);
+easy.textContent = 'Easy';
+const medium = create('div', 'medium grade', complexity);
+medium.textContent = 'Medium';
+const hard = create('div', 'hard grade', complexity);
+hard.textContent = 'Hard';
+const close = create('a', 'close-btn', complexity);
+
+function levelSwitch(event) {
+  levelList.innerHTML = '';
+  const btn = event.target;
+  const level = btn.textContent.toLowerCase();
+  const grades = complexity.querySelectorAll('.grade');
+  for (let grade of grades) {
+    grade.classList.remove('chosen');
+  }
+  btn.classList.add('chosen');
+  fillList(level);
+}
+
+function fillList(level) {
+  const arr = answers.filter((item) => item.level === level);
+  for (let i = 0; i < arr.length; i++) {
+    const level = create('div', 'level', levelList);
+    level.textContent = arr[i].name;
+    level.addEventListener('click', selectPic)
+  }
+  const levels = levelList.querySelectorAll('.level');
+  setTimeout(() => {
+    levels.forEach((elem) => elem.classList.add('visible'));
+  });
+}
+
+const grades = levelMenu.querySelectorAll('.grade');
+for (let grade of grades) {
+  grade.addEventListener('click', levelSwitch);
+}
+
+function selectPic(event) {
+  table.innerHTML = '';
+  const name = event.target.textContent;
+  const index = answers.findIndex((elem) => elem.name === name);
+  picNumber(index);
+}
+
+
+function openMenu() {
+  levelMenu.style.left = '0';
+  easy.classList.add('chosen');
+  fillList('easy');
+}
+menu.addEventListener('click', openMenu);
+
+function closeMenu() {
+  levelMenu.style.left = '-100%';
+  medium.classList.remove('chosen');
+  hard.classList.remove('chosen');
+  setTimeout(() => (levelList.innerHTML = ''), 700);
+}
+close.addEventListener('click', closeMenu);
