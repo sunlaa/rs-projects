1918724111a6f2d8b255c16e19338989f0ddbe96
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-25 11:09:27 +0100

    feat: create base for Track class

--- async-race/src/pages/garage/tracks/track/track.ts
+++ async-race/src/pages/garage/tracks/track/track.ts
@@ -1,8 +1,9 @@
-// import BaseElement from '../../../../utils/components/base-element';
-// import Car from './car/car-view';
+import BaseElement from '../../../../utils/components/base-element';
+import CarName from './car-name/car-name';
+import Car from './car/car-view';
 
-// export default class Track extends BaseElement {
-//   constructor(car: Car) {
-//     super({ className: ['track'] });
-//   }
-// }
+export default class Track extends BaseElement {
+  constructor(car: Car) {
+    super({ className: ['track'] }, car, new CarName(car.name));
+  }
+}
